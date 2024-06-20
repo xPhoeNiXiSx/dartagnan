a4b212c8acf6a3c8f67710bc7f886ef5aac87e38
tfraquet <tfraquet@a5sys.com>
2024-06-20 14:38:10 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayComponent } from './pages/game/play/play.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'game/play',
+    component: PlayComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
