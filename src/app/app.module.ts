1a6504b7f60c6d2355026364595d4fcf8f07a0a7
tfraquet <tfraquet@a5sys.com>
2024-06-23 16:05:07 +0200

    maj play

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
-import { HomeComponent } from './pages/home/home.component';
-import { PlayComponent } from './pages/game/play/play.component';
 import { AuthService } from './services/auth/auth.service';
 import { AuthGuard } from './auth.guard';
 import { SecuredLayoutComponent } from './layout/secured-layout/secured-layout.component';
@@ -15,12 +14,11 @@ import { SecuredLayoutComponent } from './layout/secured-layout/secured-layout.c
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    PlayComponent,
     SecuredLayoutComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
   ],
