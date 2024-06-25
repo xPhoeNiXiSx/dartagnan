c9712d4003cfa0958f4fd2c5dc49ca6e9148ea87
tfraquet <tfraquet@a5sys.com>
2024-06-25 13:37:50 +0200

    maj Bottom game

--- src/app/pages/game/bottom/bottom.component.ts
+++ src/app/pages/game/bottom/bottom.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-bottom',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule,
+  ],
   templateUrl: './bottom.component.html',
   styleUrl: './bottom.component.scss'
 })
